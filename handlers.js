0264f4ae6857020e97c51d654c3085f95d0177c3
rxbryan <rxbryn@gmail.com>
2022-02-18 19:12:07 +0100

    better error reporting for notFound and serverError handlerss

--- handlers.js
+++ handlers.js
@@ -1,11 +1,24 @@
+const createError = require('./libs/error')()
+
 exports.notFound = (req, res) => {
-  //console.log(req.headers)
-  res.status('404').json({message: '404 not found'})
+  for (route of createError.routes) {
+    if (route.PATH_REGEX.test(req.path)) {
+      if (!route.methods.includes(req.method.toUpperCase)) {
+        createError.errorDetails({message: `${req.method.toUpperCase()} not allowed on resource`})
+      }
+      return res.status('400').json(createError.methodNotAllowed())
+    }
+  }
+  return res.status('404').json(createError.resourceError({}))
 }
 
 /* eslint-disable no-unused-vars */
 exports.serverError = (err, req, res, next) => {
-  console.error(err.stack)
-  res.status('500').json({message: 'server error'})
+  console.error(err)
+  //properly report invalid json request body error
+  if (err.type === 'entity.parse.failed')
+    return res.status('400').json(createError.errorDetails({message: 'invalid JSON body'}).badRequestError())
+  else
+    return res.status('500').json(createError.serverError())
 }
 /* eslint-enable no-unused-vars */
