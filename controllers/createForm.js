0095c426567d214c88d3095c716b8d3926cdec78
rxbryan <rxbryn@gmail.com>
2022-02-20 01:39:32 +0100

    bug fix

--- controllers/createForm.js
+++ controllers/createForm.js
@@ -9,49 +9,44 @@ const requestError = new apiError()
 		return res.status('400').json(requestError.requestBodyError())
 	}
 
-	utils.decodeJWS(req.JWT).then(payload => {
-		let form = {}
+	let form = {}
 
-		if (!req.body.redirectFailure) {
-			requestError.errorDetails({message: 'redirectFailure undefined', target: 'invalidEntityBodyParamter'})
-		}
-		if (!req.body.redirectSuccess) {
-			requestError.errorDetails({message: 'redirectSuccess undefined', target: 'invalidEntityBodyParamter'})
-		}
-		if (requestError.errors() > 0) {
-			return res.status('400').json(requestError.invalidBodyError())
-		}
+	if (!req.body.redirectFailure) {
+		requestError.errorDetails({message: 'redirectFailure undefined', target: 'invalidEntityBodyParamter'})
+	}
+	if (!req.body.redirectSuccess) {
+		requestError.errorDetails({message: 'redirectSuccess undefined', target: 'invalidEntityBodyParamter'})
+	}
+	if (requestError.errors() > 0) {
+		return res.status('400').json(requestError.invalidBodyError())
+	}
 
-		if (req.body.fileUpload === true)	{
-			form.fileUpload = true
-		} else {
-			form.fileUpload = false
-		}
+	if (req.body.fileUpload === true)	{
+		form.fileUpload = true
+	} else {
+		form.fileUpload = false
+	}
 
-		if (req.body.status === 'enabled') {
-			form.status = req.body.status
-		} else {
-			form.status = 'disabled'
-		}
+	if (req.body.status === 'enabled') {
+		form.status = req.body.status
+	} else {
+		form.status = 'disabled'
+	}
 
-		form.formId = utils.genformId()
-		form.redirectUrl = {
-			success: req.body.redirectFailure,
-			failure: req.body.redirectSuccess
-		}
+	form.formId = utils.genformId()
+	form.redirectUrl = {
+		success: req.body.redirectFailure,
+		failure: req.body.redirectSuccess
+	}
 
-		db.storeForm(form).then(() => {
-			res.status('201').set('Location', req.headers['host']+`/forms/submit/${form.formId}`).json({
-				formId: form.formId,
-				action: `${req.headers['host']}/forms/submit/${form.formId}`
-			})
-		}).catch(error => {
-				res.status('200').json(requestError.dbError(
-					{status: 500, message: error['message'] || 'an error occurred in db#storeForm'}))
-			})
+	db.storeForm(form).then(() => {
+		res.status('201').set('Location', req.headers['host']+`/forms/submit/${form.formId}`).json({
+			formId: form.formId,
+			action: `${req.headers['host']}/forms/submit/${form.formId}`
+		})
 	}).catch(error => {
-		res.status('400').json(
-			requestError.errorDetails({message: error.message, target: 'decodeJWS'}).authenticationError())
+			res.status('200').json(requestError.dbError(
+				{status: 500, message: error['message'] || 'an error occurred in db#storeForm'})
+			)
 	})
-	
 }
